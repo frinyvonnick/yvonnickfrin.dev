f7c546f9a9aa1b763ddbe0e5670d536e07c439e8
FRIN Yvonnick <frin.yvonnick@gmail.com>
2019-12-02 09:21:45 +0100

    :lipstick: Add menu and fix mobile style for streams

--- src/gatsby-theme-blog/components/header.js
+++ src/gatsby-theme-blog/components/header.js
@@ -1,14 +1,30 @@
 import React from 'react'
+import { Link } from "gatsby"
+import { css, Styled } from "theme-ui"
 
 import Header from 'gatsby-theme-blog/src/components/header'
 
 import { SocialIcons } from '../../components/SocialIcons'
+
+import './header.css'
 
 export default props => {
   return (
     <>
       <SocialIcons />
       <Header {...props} />
+      <div
+        css={css({
+          position: 'relative',
+          maxWidth: `container`,
+          mx: `auto`,
+        })}
+      >
+        <nav>
+          <Link className={!props.location.pathname.includes('/streams') ? 'selected' : ''} to="/">Blog</Link>
+          <Link className={props.location.pathname.includes('/streams') ? 'selected' : ''} to="/streams">Streaming</Link>
+        </nav>
+      </div>
     </>
   )
 }
