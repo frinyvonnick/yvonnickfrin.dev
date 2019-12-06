af9e39e13b9f24c0dcd80134ab4f1a8e1afc4cee
FRIN Yvonnick <frin.yvonnick@gmail.com>
2019-12-06 14:43:07 +0100

    :recycle: Use node-html-to-image

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,10 +1,9 @@
 const path = require(`path`)
 const { promisify } = require('util')
 const fs = require(`fs`)
-const handlebars = require('handlebars')
-const puppeteer = require('puppeteer')
 const request = require('request').defaults({ encoding: null });
 const { fr } = require('date-fns/locale')
+const nodeHtmlToImage = require('node-html-to-image')
 const { format } = require('date-fns')
 
 const existsAsync = promisify(fs.exists)
@@ -57,13 +56,8 @@ exports.onCreateNode = async ({ node, actions }) => {
   if (node.internal.type === 'MarkdownRemark') {
     const { frontmatter } = node 
 
-    const source = fs.readFileSync(path.join(__dirname, './src/templates/stream-card-template.html')).toString('utf8')
-    const template = handlebars.compile(source)
-    const browser = await puppeteer.launch({ headless: true, args: ['--lang=fr-FR,fr'] })
-    const page = await browser.newPage()
-
     const [hours, minutes] = frontmatter.startHour.split(':')
-    const pageContent = {
+    const content = {
       title: frontmatter.title,
       streamer: 'https://avatars.io/twitter/yvonnickfrin',
       date: format(new Date(frontmatter.date), 'eeee dd MMMM yyyy', { locale: fr }),
@@ -71,14 +65,16 @@ exports.onCreateNode = async ({ node, actions }) => {
     }
 
     if (frontmatter.guest) {
-      pageContent.guest = `https://avatars.io/twitter/${frontmatter.guest}`
+      content.guest = `https://avatars.io/twitter/${frontmatter.guest}`
     }
 
-    await page.setContent(template(pageContent))
-    const elements = await page.$$('body')
-    const element = elements[0]
-    await element.screenshot({ path: `./public${frontmatter.path}.jpg` })
-
-    await browser.close()
+    const html = fs.readFileSync(path.join(__dirname, './src/templates/stream-card-template.html')).toString('utf8')
+    await nodeHtmlToImage({
+      html,
+      output: `./public${frontmatter.path}.jpg`,
+      content,
+      type: 'jpeg',
+      puppeteerArgs: { args: ['--lang=fr-FR,fr'] }
+    })
   }
 }
