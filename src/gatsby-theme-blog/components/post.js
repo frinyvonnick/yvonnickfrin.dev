e0e7bfa6a21365221fc6cb4e782b90dda4a94f37
FRIN Yvonnick <frin.yvonnick@gmail.com>
2020-01-24 11:58:29 +0100

    :sparkles: Add related projects in blog posts

--- src/gatsby-theme-blog/components/post.js
+++ src/gatsby-theme-blog/components/post.js
@@ -1,6 +1,6 @@
 import React from "react"
-import { Styled, css } from "theme-ui"
-import { useStaticQuery, graphql } from "gatsby"
+import { Styled, css, useColorMode } from "theme-ui"
+import { useStaticQuery, graphql, Link } from "gatsby"
 
 import PostFooter from "gatsby-theme-blog/src/components/post-footer"
 import Layout from "../components/layout"
@@ -18,7 +18,9 @@ const Post = ({
   previous,
   next,
 }) => {
-  const { allBlogPost: { edges } } = useStaticQuery(
+  const [colorMode] = useColorMode()
+  const isDark = colorMode === `dark`
+  const { allBlogPost: { edges: posts }, allGithubProject: { edges: projects } } = useStaticQuery(
     graphql`
       query getPostData {
         allBlogPost {
@@ -26,14 +28,28 @@ const Post = ({
             node {
               id
               slug
+              tags
+            }
+          }
+        }
+        allGithubProject {
+          edges {
+            node {
+              id
+              name
+              description
             }
           }
         }
       }
     `
   )
-  const { node: { slug } } = edges.find(({ node }) => node.id === post.id)
+  const { node: { slug, tags } } = posts.find(({ node }) => node.id === post.id)
   const editLink = `https://github.com/frinyvonnick/yvonnickfrin.dev/edit/master/content/posts${slug}.mdx`
+
+  const relatedProjects = projects
+    .map(({ node }) => node)
+    .filter(({ name }) => tags.includes(name))
 
   return (
     <Layout location={location} title={title}>
@@ -64,6 +80,53 @@ const Post = ({
         </Styled.div>
       </main>
       <PostFooter {...{ previous, next }} />
+      <Styled.h2
+        css={css({
+          marginTop: `40px`,
+        })}
+      >
+        Related projects
+      </Styled.h2>
+      {relatedProjects.map(project => (
+        <article key={project.id}>
+          <Styled.h3
+            css={css({
+              marginBottom: 0,
+            })}
+          >
+            <Styled.a
+              as={Link}
+              css={css({
+                color: `inherit`,
+              })}
+              to={`/projects/${project.name}`}
+            >
+              {project.name}
+            </Styled.a>
+          </Styled.h3>
+          <p>{project.description}</p>
+          <Styled.div
+            css={css({
+              display: 'flex',
+              justifyContent: 'flex-end',
+            })}
+          >
+            <Styled.a
+              as={Link}
+              css={css({
+                boxShadow: `none`,
+                textDecoration: `none`,
+                color: isDark ? `#232129` : `white`,
+                backgroundColor: `primary`,
+                padding: `5px 15px`,
+              })}
+              to={`/projects/${project.name}`}
+            >
+              Read more
+            </Styled.a>
+          </Styled.div>
+        </article>
+      ))}
     </Layout>
   )
 }
