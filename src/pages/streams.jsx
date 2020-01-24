f93aae2c4af0ee287ba0d5186b12f36d000030ad
FRIN Yvonnick <frin.yvonnick@gmail.com>
2020-01-24 15:20:01 +0100

    :pencil: Add a description to streams page

--- src/pages/streams.jsx
+++ src/pages/streams.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Styled, css } from 'theme-ui'
 import { Helmet } from "react-helmet"
 import { graphql } from 'gatsby'
 
@@ -29,6 +30,12 @@ export default function({ data, ...props }) {
       <Helmet>
         <html lang="fr" />
       </Helmet>
+      <Styled.p css={css({ textAlign: 'justify' })}>
+        Je streame principalement sur des technologies JavaScript. J'aime beaucoup l'open source je contribue donc sur quelques projets comme <Styled.a href="https://github.com/frinyvonnick/gitmoji-changelog">gitmoji-changelog</Styled.a>, <Styled.a href="https://github.com/charlyx/pair-roulette">pair-roulette</Styled.a>, <Styled.a href="https://github.com/bpetetot/conference-hall">conference-hall</Styled.a>, ...
+      </Styled.p>
+      <Styled.p css={css({ textAlign: 'justify' })}>
+        Habituellement je streame le dimanche de 16h à 18h et le mardi de 20h à 22h. N'hésitez pas à suivre <Styled.a href="https://www.twitch.tv/yvonnickfrin">ma chaine Twitch</Styled.a> pour être averti lorsque le stream commence.
+      </Styled.p>
       <div className="streams">
         <h2>Prochain stream</h2>
         {nextStream ? (
