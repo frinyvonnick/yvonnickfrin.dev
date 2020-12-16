2a801478e6b81fb7097ff12275352e0dd28da4bf
Yvonnick FRIN <yvonnickfrin@macbook-pro-de-yvonnick.home>
2020-12-16 14:47:38 +0100

    Move to gatsby blog new version

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,20 +4,38 @@ module.exports = {
       resolve: `gatsby-theme-blog`,
       options: {},
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-66760805-2",
+        anonymize: true,
+        respectDNT: true,
+      },
+    },
+    'gatsby-plugin-sitemap',
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        host: 'https://yvonnickfrin.dev',
+        sitemap: 'https://yvonnickfrin.dev/sitemap.xml',
+        policy: [{ userAgent: '*', allow: '/' }]
+      }
+    }
   ],
   // Customize your site metadata:
   siteMetadata: {
-    title: `My Blog Title`,
-    author: `My Name`,
-    description: `My site description...`,
+    siteUrl: 'https://yvonnickfrin.dev',
+    title: `Yvonnick Frin`,
+    author: `Yvonnick Frin`,
+    description: `Articles, Streams and Talks about JavaScript and its ecosystem.`,
     social: [
       {
         name: `twitter`,
-        url: `https://twitter.com/gatsbyjs`,
+        url: `https://twitter.com/YvonnickFrin`,
       },
       {
         name: `github`,
-        url: `https://github.com/gatsbyjs`,
+        url: `https://github.com/frinyvonnick`,
       },
     ],
   },
--- src/gatsby-theme-blog/components/bio-content.js
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -1,18 +1,12 @@
 import React from "react"
 import { Styled } from "theme-ui"
 
-/**
- * Change the content to add your own bio
- */
-
 export default function Bio() {
   return (
-    <>
-      This is where <Styled.a href="http://example.com/">your name</Styled.a>
-      {` `}
-      goes.
-      <br />
-      Or whatever, you make the rules.
-    </>
+  <>
+    Senior frontend engineer at <Styled.a href="http://twitter.com/sterblue">@Sterblue</Styled.a>.
+    <br />
+    I'm a <Styled.a href="http://twitter.com/NantesJS">@NantesJS</Styled.a> co-organizer on my spare time.
+  </>
   )
 }
