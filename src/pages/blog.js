f8269e5c2c986b8299247d9a65963d18f98d1ade
Yvonnick Frin <frin.yvonnick@gmail.com>
2021-08-27 12:36:00 +0200

    Sort blog posts

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -7,21 +7,34 @@ import Post from "../components/Post"
 const Blog = ({ data }) => (
   <Layout>
     <h1>Blog</h1>
-    {data.allMarkdownRemark.edges.map(post => {
-      const { excerpt } = post.node
-      const { title, date } = post.node.frontmatter
-      const { id, name } = post.node.parent
+    {data.allMarkdownRemark.edges
+      .sort((a, b) => {
+        const date1 = new Date(a.node.frontmatter.date)
+        const date2 = new Date(b.node.frontmatter.date)
 
-      return (
-        <Post
-          key={id}
-          title={title}
-          date={date}
-          excerpt={excerpt}
-          path={`/${name}`}
-        />
-      )
-    })}
+        if (date1 > date2) {
+          return -1
+        } else if (date1 < date2) {
+          return 1
+        } else {
+          return 0
+        }
+      })
+      .map(post => {
+        const { excerpt } = post.node
+        const { title, date } = post.node.frontmatter
+        const { id, name } = post.node.parent
+
+        return (
+          <Post
+            key={id}
+            title={title}
+            date={date}
+            excerpt={excerpt}
+            path={`/${name}`}
+          />
+        )
+      })}
   </Layout>
 )
 
