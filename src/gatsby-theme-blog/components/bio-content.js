8fe8fb2e37e53e212cc2996fbba07837792ae1a4
Yvonnick FRIN <frin.yvonnick@gmail.com>
2022-04-21 12:26:14 +0200

    :memo: Update job in bio

--- src/gatsby-theme-blog/components/bio-content.js
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -3,7 +3,7 @@ import { Styled } from "theme-ui"
 
 export default () => (
   <Fragment>
-    Open source advocate at <Styled.a href="http://twitter.com/ZenikaIT">@ZenikaIT</Styled.a>.
+    Fullstack developer at <Styled.a href="https://twitter.com/Pix_officiel">@Pix_officiel</Styled.a>.
     <br />
     I'm a <Styled.a href="http://twitter.com/NantesJS">@NantesJS</Styled.a> co-organizer on my spare time.
   </Fragment>
